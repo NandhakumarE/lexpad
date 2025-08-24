3cbeeaac3b0c1ee1824600ab4150bbcf2b3e596d
Nandhakumar <nandhakumardev97@gmail.com>
2025-08-24 19:10:11 +0530

    fix: Added more customization in Editor component

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,20 @@ function App() {
   const [html, setHtml] = useState<string>("");
 
   return (
-    <div>
+    <div className="h-[100vh] flex items-center justify-center">
       <RichTextEditor
         name="text-editor"
         placeholder="Provide your content here..."
         value={html}
         onChange={(value) => setHtml(value)}
+        autoFocus
+        // readOnly
+        debounce={300}
+        className=" w-[70vw] !border-0 gap-4 "
+        toolbarClassName="shadow-lg rounded-xl bg-white flex-wrap"
+        editorContainerClassName="shadow-xl rounded-xl bg-white"
+        editorClassName="min-h-[50vh]"
+        plugins={[]}
       />
     </div>
   );
